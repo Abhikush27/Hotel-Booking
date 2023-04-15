1e952df72fa0b42dcedc37c49a92c4f23ec80efc
Abhijeet Chawla <abhijeetchawla33@gmail.com>
2023-04-15 23:02:18 +0530

    Added

--- client/src/UserAccount/PlacePage.js
+++ client/src/UserAccount/PlacePage.js
@@ -1,106 +1,160 @@
-import React,{useState} from 'react'
-import {Link} from "react-router-dom"
-import './PlacePage.css'
-import { useParams } from 'react-router-dom'
-import { Button } from '@mui/material'
-import axios from 'axios'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./PlacePage.css";
+import { useParams } from "react-router-dom";
+import { Button } from "@mui/material";
+import axios from "axios";
 // import AddIcon from '@mui/icons-material/Add';
 
 function PlacePage() {
+  const { action } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [Photos, setPhotos] = useState([]);
+  const [description, setdescription] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuest, setMaxGuest] = useState(1);
+  const [redirect, setRedirect] = useState("");
 
-const {action} = useParams();
-const [title,setTitle]=useState('');
-const [address,setAddress]=useState('');
-const [Photos,setPhotos]=useState([]);
-const [description,setdescription]=useState('');
-const [checkIn,setCheckIn]=useState('');
-const [checkOut,setCheckOut]=useState('');
-const [maxGuest,setMaxGuest]=useState(1);
+
+  function addPhotoByLink(event) {
+    event.preventDefault();
+    const files = event.target.files;
+    const data = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      data.append("photos", files[i]);
+    }
+
+    console.log("hello");
+    axios
+      .post("/upload", data, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then((response) => {
+        const { data: filenames } = response;
+        setPhotos((prev) => {
+          return [...prev, ...filenames];
+        });
+      });
+  }
 
 
- function addPhotoByLink(event){
-  event.preventDefault()
-const files = event.target.files;
-const data = new FormData();
+  async function addNewPlaces(ev) {
+    ev.preventDefault();
+    const placeData = {title,address,Photos,description,checkIn,checkOut,maxGuest, };
+    await axios.post("/places", placeData);
 
-for(let i=0;i<files.length;i++){
-  data.append('photos',files[i]);
-}
-
-console.log("hello")
-axios.post('/upload',data,{
-  headers:{'Content-type':'multipart/form-data'}
-}).then(response =>{
-  const{data:filenames} = response;
-  setPhotos(prev =>{
-    return [...prev, ...filenames];
-  })
-})
-}
-
+    setRedirect('/account/places');
+  }
 
   return (
-
-   <div>
-    {action !=='addplaces' && (
-<div className='add-place'>
-    <Link className='addicon' to ={'/account/places/addplaces'}>  Add Places
-     {/* <AddIcon/> */}
-    </Link>
-   
-</div>
-
-)}
-
-{action ==='addplaces' &&(
-   <div>
-    <form onSubmit={e=>e.preventDefault()} className='places-form'>
-      <h3>Title</h3>
-      <p>title for your place</p>
-      <input type="text" value ={title} onChange = {(e)=>setTitle(e.target.value)} style={{height: "4vh",width:"92%"}} placeholder='title' />
-      <h3>Address</h3>
-      <p>Address to this place</p>
-      <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} style={{height: "4vh",width:"92%"}} placeholder='address' />
-      <h3>Photos</h3>
-
-      <div>
-        <label value={Photos} onChange={(e)=>setPhotos(e.target.value)} className='upload-photos'>
-          <input type="file" multiple className='' onChange={addPhotoByLink} placeholder='Add image using "Link"' />
-         <button style={{ color:"black", justifyContent:"center",textAlign:"center"}}>Upload</button>
-          
-          </label>
-      </div>
-
-      <h3>Description</h3>
-      <textarea 
-      value={description} onChange={(e)=>setdescription(e.target.value)}
-      cols="90" rows="8"></textarea>
-
-      <h3>Check in&out times</h3>
-      <p>add checkin and checkout time</p>
-
-      <div className='checkin-out'>
-        <div className='checkin-input'>
-          <h5>Checkin time</h5>
-          <input type="text" value={checkIn} onChange={(e)=>setCheckIn(e.target.value)} />
+    <div>
+      {action !== "addplaces" && (
+        <div className="add-place">
+          <Link className="addicon" to={"/account/places/addplaces"}>
+            {" "}
+            Add Places
+            {/* <AddIcon/> */}
+          </Link>
         </div>
-        <div className='checkin-input'>
-          <h5>Checkin time</h5>
-          <input type="text" value={checkOut} onChange={(e)=>setCheckOut(e.target.value)}/>
-        </div>
-        <div className='checkin-input'>
-          <h5>Checkin time</h5>
-          <input type="text" value={maxGuest} onChange={(e)=>setMaxGuest(e.target.value)}/>
-        </div>
-      </div>
+      )}
 
-<Button style={{background:"red",color:'white'}}>Save</Button>
+      {action === "addplaces" && (
+        <div>
+          <form onSubmit={addNewPlaces} className="places-form">
+            <h3>Title</h3>
+            <p>title for your place</p>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              style={{ height: "4vh", width: "92%" }}
+              placeholder="title"
+            />
+            <h3>Address</h3>
+            <p>Address to this place</p>
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              style={{ height: "4vh", width: "92%" }}
+              placeholder="address"
+            />
+            <h3>Photos</h3>
 
-    </form>
-   </div>
-)}
-   </div>
-  )
+            {/* upload photo functionality */}
+            <div>
+              <label
+                value={Photos}
+                onChange={(e) => setPhotos(e.target.value)}
+                className="upload-photos"
+              >
+                <input
+                  type="file"
+                  multiple
+                  className=""
+                  onChange={addPhotoByLink}
+                  placeholder='Add image using "Link"'
+                />
+                <button
+                  style={{
+                    color: "black",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  Upload
+                </button>
+              </label>
+            </div>
+
+            <h3>Description</h3>
+            <textarea
+              value={description}
+              onChange={(e) => setdescription(e.target.value)}
+              cols="90"
+              rows="8"
+            ></textarea>
+
+            <h3>Check in&out times</h3>
+            <p>add checkin and checkout time</p>
+
+            {/* check in check out */}
+            <div className="checkin-out">
+              <div className="checkin-input">
+                <h5>Checkin time</h5>
+                <input
+                  type="text"
+                  value={checkIn}
+                  onChange={(e) => setCheckIn(e.target.value)}
+                />
+              </div>
+              <div className="checkin-input">
+                <h5>Checkin time</h5>
+                <input
+                  type="text"
+                  value={checkOut}
+                  onChange={(e) => setCheckOut(e.target.value)}
+                />
+              </div>
+              <div className="checkin-input">
+                <h5>Checkin time</h5>
+                <input
+                  type="text"
+                  value={maxGuest}
+                  onChange={(e) => setMaxGuest(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <Button style={{ background: "red", color: "white" }}>Save</Button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default PlacePage
+export default PlacePage;
--- server/models/placemodel.js
+++ server/models/placemodel.js
@@ -1,21 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const placeSchema = mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjetId, ref: "User" },
 
-owner:{type:mongoose.Schema.Types.ObjetId, ref:'User'},
-
-title:{type:String},
-address:{type:String},
-description:{type:String},
-photos:{type:[String]},
-// extraInfo:{type:String},
-chekIn:{type:Number},
-checkout:{type:Number},
-maxGuests:{type:Number},
-
+  title: { type: String },
+  address: { type: String },
+  description: { type: String },
+  photos: { type: [String] },
+  // extraInfo:{type:String},
+  chekIn: { type: Number },
+  checkout: { type: Number },
+  maxGuests: { type: Number },
 });
 
-const PlaceModel = mongoose.model('Place',placeSchema);
+const PlaceModel = mongoose.model("Place", placeSchema);
 
 module.exports = PlaceModel;
-
